2a0f8dd9a3d5573714353345bc6daa26ffa373e8
Tijs <tteulings@gmail.com>
2011-09-26 14:38:06 +0200

    changed some specifics to generic settings

--- settings/local.py
+++ settings/local.py
@@ -7,8 +7,8 @@ TEMPLATE_DEBUG=DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'iknewit',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
+        'NAME': 'DATABASE',                      # Or path to database file if using sqlite3.
+        'USER': 'USER',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
